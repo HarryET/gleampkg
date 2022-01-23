1f5e123ab56848c751e37554e0621f9f9362257b
HarryET <29015545+HarryET@users.noreply.github.com>
2022-01-23 15:34:48 +0000

    Update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import { NextPage } from "next"
 
 const index: NextPage = () => {
   return (
-    <div className={"w-full h-full flex items-center justify-center"}>
-        <p>Nothing to see here!</p>
+    <div className={"w-full h-full flex flex-col items-center justify-center"}>
+      <p className="text-2xl font-bold text-black">Work in progress</p>
+      <p className="text-gray-700">There is no UI just yet but feel free to use the API</p>
+      <div className={"flex flex-col items-center justify-center mt-4"}>
+        <code>https://gleampkg.com/api/packages/:name</code>
+        <code>https://gleampkg.com/api/packages/:name/:version</code>
+      </div>
     </div>
   )
 }
