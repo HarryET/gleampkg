58bb8690e49700971fcfba64c82ec9fdeed22598
HarryET <29015545+HarryET@users.noreply.github.com>
2022-01-23 17:20:47 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
 const index: NextPage = () => {
   return (
     <div className={"w-full h-full flex flex-col items-center justify-center"}>
-      <p className="text-2xl font-bold text-black">Work in progress</p>
+      <p className="text-2xl font-bold text-black mb-1">Work in progress</p>
       <p className="text-gray-700">There is no UI just yet but feel free to use the API</p>
       <div className={"flex flex-col items-center justify-center mt-4"}>
         <code>https://gleampkg.com/api/packages/:name</code>
